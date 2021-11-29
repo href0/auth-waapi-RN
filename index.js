bd8f4a714d930299b73d2d1b322cb5efe7964c53
hrefdev <href.dev@gmail.com>
2021-11-29 17:38:22 +0700

    update

--- index.js
+++ index.js
@@ -5,5 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import Dashboard from './src/Dashboard';
 
 AppRegistry.registerComponent(appName, () => App);
